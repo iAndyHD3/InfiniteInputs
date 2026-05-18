295f2ff212b8317f82730b0757a6008022c1a128
iAndyHD3 <54410739+iAndyHD3@users.noreply.github.com>
2026-05-18 22:44:35 +0200

    make it work on mac

--- src/hooks/PlatformToolbox.cpp
+++ src/hooks/PlatformToolbox.cpp
@@ -1,10 +1,10 @@
+#if defined(GEODE_IS_WINDOWS)
+
 #include <Geode/modify/CCEGLView.hpp>
-#include "BetterGeodeLogs.hpp"
 #include "GJBaseGameLayer.hpp"
 #include "Geode/cocos/platform/win32/CCEGLView.h"
 
 using namespace geode::prelude;
-
 class $modify(CCEGLView)
 {
     void toggleLockCursor(bool isLocked) {
@@ -19,5 +19,23 @@ class $modify(CCEGLView)
     }
 };
 
+#elif defined(GEODE_IS_MACOS)
 
+#include <Geode/modify/PlatformToolbox.hpp>
+#include "GJBaseGameLayer.hpp"
 
+class $modify(PlatformToolbox)
+{
+    void toggleLockCursor(bool isLocked) {
+        //this is way easier and simpler than manually toggling it everywhere.
+        //its actually called before playlayer is initialized, so at the start it gets called manually in gjbgl
+        auto mylayer = reinterpret_cast<MyBaseLayer*>(PlayLayer::get());
+        if(mylayer && mylayer->m_fields->hasAnyMouseKeyActive()) {
+            PlatformToolbox::toggleLockCursor(false);
+            return;
+        }
+        PlatformToolbox::toggleLockCursor(isLocked);
+    }
+};
+
+#endif
